1a13c7d1857c8ca1a07b62460ab835fcd0df2d9b
Nicola Abello <nicola@infielddesign.com>
2016-01-12 12:54:47 -0800

    Fixed script

--- get_packages-6_0.sh
+++ get_packages-6_0.sh
@@ -2,7 +2,6 @@
 #Destination
 DEST='./ansible-aem/roles/aem-install/files'
 
-#Core
-#Vulnerability in the Apache POI Library (Hot fix 6449 *)
-HF_6449='https://www.adobeaemcloud.com/content/companies/public/adobe/packages/aem610/hotfix/cq-6.1.0-hotfix-6449/jcr%3acontent/package/file.res/cq-6.1.0-hotfix-6449-1.2.zip'
-wget $HF_6449 -P $DEST
+#SP3
+SP3='https://www.adobeaemcloud.com/content/companies/public/adobe/packages/cq600/servicepack/AEM-6.0-Service-Pack-3/jcr%3acontent/package/file.res/AEM%206.0%20Service%20Pack%203-6.0.SP3.zip'
+wget $SP3 -P $DEST
