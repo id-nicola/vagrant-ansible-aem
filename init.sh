2badbb403f6e10bbf89f315fc23b2c1c4b4173d2
Nicola Abello <nicola@infielddesign.com>
2016-01-12 11:08:32 -0800

    Update names of files.

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 sudo chmod 775 ansible-aem/init.sh
-sudo chmod 775 get_packages.sh
+sudo chmod 775 get_packages-6_0.sh
+sudo chmod 775 get_packages-6_1.sh
 cp nodes.json.default nodes.json
 ./ansible-aem/init.sh
