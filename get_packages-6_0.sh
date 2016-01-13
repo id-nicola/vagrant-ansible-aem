c7ac3f911909d43c272f36f672627e7a3751175e
Nicola Abello <nicola@infielddesign.com>
2016-01-13 08:16:37 -0800

    Moving package scripts over to ansible-aem repository.

--- get_packages-6_0.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#Destination
-DEST='./ansible-aem/roles/aem-install/files/'
-
-#SP3
-SP3='https://www.adobeaemcloud.com/content/companies/public/adobe/packages/cq600/servicepack/AEM-6.0-Service-Pack-3/jcr%3acontent/package/file.res/AEM%206.0%20Service%20Pack%203-6.0.SP3.zip'
-wget $SP3 -P $DEST
-mv $DEST'AEM 6.0 Service Pack 3-6.0.SP3.zip'  $DEST'AEM-6.0-Service-Pack-3-6.0.SP3.zip'
--- init.sh
+++ init.sh
@@ -1,27 +1,4 @@
 #!/usr/bin/env bash
 sudo chmod 775 ansible-aem/init.sh
-sudo chmod 775 get_packages-6_0.sh
-sudo chmod 775 get_packages-6_1.sh
-
-PS3='Install AEM version : '
-options=("AEM 6.0 [1]" "AEM 6.1 [2]" "Quit [3]")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "AEM 6.0 [1]")
-            ./get_packages-6_0.sh
-            break
-        ;;
-        "AEM 6.1 [2]")
-            ./get_packages-6_1.sh
-            break
-        ;;
-        "Quit [3]")
-            exit
-        ;;
-        *) echo Invalid option;;
-    esac
-done
-
 cp nodes.json.default nodes.json
 ./ansible-aem/init.sh
