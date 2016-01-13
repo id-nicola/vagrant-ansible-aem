456346233a7661a093c46c9d2368187ee7c30c64
Nicola Abello <nicola@infielddesign.com>
2016-01-12 16:46:06 -0800

    No white spaces.

--- get_packages-6_0.sh
+++ get_packages-6_0.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #Destination
-DEST='./ansible-aem/roles/aem-install/files/AEM-6.0-Service-Pack-3-6.0.SP3.zip'
+DEST='./ansible-aem/roles/aem-install/files/'
 
 #SP3
 SP3='https://www.adobeaemcloud.com/content/companies/public/adobe/packages/cq600/servicepack/AEM-6.0-Service-Pack-3/jcr%3acontent/package/file.res/AEM%206.0%20Service%20Pack%203-6.0.SP3.zip'
-wget $SP3 -P $DEST
-mv $DEST
+wget $SP3 -r $DEST
+mv $DEST'AEM 6.0 Service Pack 3-6.0.SP3.zip'  $DEST'AEM-6.0-Service-Pack-3-6.0.SP3.zip'
