96550d166a66b58b61a643ffcd42050ddc842a39
Nicola Abello <nicola@infielddesign.com>
2016-01-12 16:25:04 -0800

    Renaming SP3 package.

--- get_packages-6_0.sh
+++ get_packages-6_0.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #Destination
-DEST='./ansible-aem/roles/aem-install/files'
+DEST='./ansible-aem/roles/aem-install/files/AEM-6.0-Service-Pack-3-6.0.SP3.zip'
 
 #SP3
 SP3='https://www.adobeaemcloud.com/content/companies/public/adobe/packages/cq600/servicepack/AEM-6.0-Service-Pack-3/jcr%3acontent/package/file.res/AEM%206.0%20Service%20Pack%203-6.0.SP3.zip'
 wget $SP3 -P $DEST
+mv $DEST
