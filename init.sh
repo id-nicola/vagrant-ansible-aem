4bcf0501de5297d031a0a148ab92aff030cc214e
Nicola Abello <nicola@infielddesign.com>
2016-01-12 16:22:07 -0800

    Prompting for AEM version and executing package script.

--- init.sh
+++ init.sh
@@ -2,5 +2,24 @@
 sudo chmod 775 ansible-aem/init.sh
 sudo chmod 775 get_packages-6_0.sh
 sudo chmod 775 get_packages-6_1.sh
+
+PS3='Install AEM version : '
+options=("AEM 6.0 [1]" "AEM 6.1 [2]" "Quit [3]")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "AEM 6.0 [1]")
+            ./get_packages-6_0.sh
+        ;;
+        "AEM 6.1 [2]")
+            get_packages-6_1.sh
+        ;;
+        "Quit [4]")
+            exit
+        ;;
+        *) echo Invalid option;;
+    esac
+done
+
 cp nodes.json.default nodes.json
 ./ansible-aem/init.sh
